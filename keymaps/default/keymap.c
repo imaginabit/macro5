23664e6d6b15630efe30329839f1af096193837b
Fernando <imaginabit@gmail.com>
2024-10-13 04:30:45 +0100

    funcionando pero plano

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -1,11 +1,8 @@
-// Copyright 2024 Your Name (@yourgithub)
-// SPDX-License-Identifier: GPL-2.0-or-later
-
 #include QMK_KEYBOARD_H
 #define DEFAULT_KEYMAP custom_keymap
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(
+    [0] = LAYOUT_linear_5(
         KC_1, KC_2, KC_3, KC_4, KC_5
     )
 };
--- keymaps/vial/keymap.c
+++ keymaps/vial/keymap.c
@@ -1,12 +1,11 @@
-// Copyright 2024 Your Name (@yourgithub)
+// Copyright 2024 Fernando Ramirez (@imaginabit)
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
 #define DEFAULT_KEYMAP custom_keymap
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(
-        KC_1, KC_2,
-        KC_6, KC_7, KC_8
+    [0] = LAYOUT_linear_5(
+        KC_1, KC_2, KC_3, KC_4, KC_5
     )
 };
